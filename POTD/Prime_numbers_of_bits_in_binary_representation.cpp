f5d00b1470523312fdc7e5e465cf7a9c5bbeb06e
Anant Srivastava <anantarivastavahp@gmail.com>
2026-02-21 16:21:10 +0530

    for prime:bool isPrime(int n) {         if (n < 2)         return false;
            for (int i = 2; i <= sqrt(n); i++) {
                if (n % i == 0)
                    return false;
            }
            return true;
        }

---
+++ POTD/Prime_numbers_of_bits_in_binary_representation.cpp
@@ -0,0 +1,30 @@
+iclass Solution {
+public:
+
+    bool isPrime(int n) {
+        if (n < 2) 
+        return false;
+
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int countPrimeSetBits(int left, int right) {
+
+        int count = 0;
+
+        for (int i = left; i <= right; i++) {
+
+            int setBits = __builtin_popcount(i);
+
+            if (isPrime(setBits))
+                count++;
+        }
+
+        return count;
+    }
+};
+
