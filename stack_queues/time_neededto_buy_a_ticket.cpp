0f9bfe1c3f11dcaa6991b3b7c555a831a3f7918b
Anant Srivastava <anantarivastavahp@gmail.com>
2026-02-28 16:53:40 +0530

    DSA

---
+++ stack_queues/time_neededto_buy_a_ticket.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+        int time = 0;
+        int target = tickets[k];
+
+        for(int i = 0; i < tickets.size(); i++){
+            if(i <= k)
+                time += min(tickets[i], target);
+            else
+                time += min(tickets[i], target - 1);
+        }
+
+        return time;
+    }
+};
+
