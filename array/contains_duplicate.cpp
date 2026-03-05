6f45fdc731eacf1a312bb5b8c1f9a863786da0e4
Anant Srivastava <anantarivastavahp@gmail.com>
2026-03-05 18:32:12 +0530

    DSA

---
+++ array/contains_duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(s.count(nums[i]) == 1) {
+                return true;
+            }
+
+            s.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
