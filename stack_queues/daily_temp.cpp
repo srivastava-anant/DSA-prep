194379a87187ae9f1b6218795500d083538688d9
Anant Srivastava <anantarivastavahp@gmail.com>
2026-03-12 17:44:09 +0530

    DSA

---
+++ stack_queues/daily_temp.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        stack<pair<int,int>> st;
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() && temperatures[i] > st.top().first) {
+
+                int prevTemp = st.top().first;
+                int prevIndex = st.top().second;
+                st.pop();
+
+                ans[prevIndex] = i - prevIndex;
+            }
+
+            st.push({temperatures[i], i});
+        }
+
+        return ans;
+    }
+};
