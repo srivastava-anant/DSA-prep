a12fc4b1b648d3717e1be7eb639fe139eb388017
Anant Srivastava <anantarivastavahp@gmail.com>
2026-04-18 15:37:42 +0530

    DSA

---
+++ linkedlist/remove_duplicate.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+        if(head == NULL){
+            return head;
+        }
+        while(curr->next != NULL && curr!=NULL){
+            if(curr->val == curr->next->val){
+                ListNode* deletednode = curr->next;
+                curr->next = curr->next->next;
+                delete(deletednode);
+            }
+            else
+            curr = curr->next;
+        }
+
+        return head;
+    }
+};
